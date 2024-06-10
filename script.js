6125ddfbd65c46eaac03936803e841c4cd652ddc
vivian_figueiredo <110409163+vivi995@users.noreply.github.com>
2024-06-10 11:01:11 -0300

    inteligencia artificial 2 trimestre

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const caixaPrincipal = document. querySelector(".caixa-principal");
-const caixaPerguntas = document. querySelector(".caixa-perguntas");
-const caixaAlternativas = document. querySelector(".caixa-Alternativas");
-const caixaResultado = document. querySelector(".caixa-resultado");
-const textoResultado= document. querySelector(".texto-resultado);
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado);
+
+    const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
