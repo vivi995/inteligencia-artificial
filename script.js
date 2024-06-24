8efbe4904163061cca9f7f1c216b63c69a628d6f
vivian_figueiredo <110409163+vivi995@users.noreply.github.com>
2024-06-24 11:30:09 -0300

    inteligencia2trimestre

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ const perguntas = [
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
+            {
+                texto: 
+                afirmacao: 
+            },
+            {
+                texto: 
+                afirmacao: 
+            }
+
             "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
         ]
@@ -28,6 +37,15 @@ const perguntas = [
     {
         enunciado: "Após a elaboração do trabalho escrito, o professor realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
+            {
+                texto: 
+                afirmacao: 
+            },
+            {
+                texto: 
+                afirmacao: 
+            }
+
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
         ]
@@ -35,6 +53,14 @@ const perguntas = [
     {
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
+            {
+                texto: 
+                afirmacao: 
+            },
+            {
+                texto: 
+                afirmacao: 
+            }
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
             "Criar uma imagem utilizando um gerador de imagem de IA."
         ]
@@ -42,7 +68,15 @@ const perguntas = [
     {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
-            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            {
+                texto: 
+                afirmacao: 
+            },
+            {
+                texto: 
+                afirmacao: 
+            }
+     "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
         ]
     }
